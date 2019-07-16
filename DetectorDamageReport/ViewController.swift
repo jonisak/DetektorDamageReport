2396a49e740e18f820d407a9a3fb18c9c49136a7
Jonas <jonisak@yahoo.com>
2019-07-16 11:52:50 +0200

    Initial Commit

---
+++ DetectorDamageReport/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DetectorDamageReport
+//
+//  Created by Jonas Isaksson on 2019-07-16.
+//  Copyright © 2019 Jonas Isaksson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
